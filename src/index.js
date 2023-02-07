8c3de7ee58ac3bfacce76145bca663aac40c0578
Charles Maria Joseph <mcharles@rippling.com>
2023-02-07 08:22:17 +0530

    modify index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("hello")
+function addNumbers(a, b) {
+    let result = a + b;
+    console.log(result);
+    return result;
+}
+  
+function calcTax(price) {
+    let tax = 0.1;
+    return price * tax;
+}
+  
+function processOrder(price) {
+let orderTotal = price + calcTax(price);
+return orderTotal;
+}
+
+let orderPrice = 100;
+let finalPrice = processOrder(orderPrice);
+console.log(finalPrice);
+  
